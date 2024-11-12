fc3adf61855835f3235d591a43d62f366e0e8008
Areeb <areeb.g24586@iqra.edu.pk>
2024-11-12 06:09:47 +0500

    update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-uncache(){
 wp cache flush --skip-plugins --skip-themes --allow-root --all
 wp elementor flush_css
 redis-cli flushall
 wp breeze purge --cache=all
-}
 echo "hello there"
